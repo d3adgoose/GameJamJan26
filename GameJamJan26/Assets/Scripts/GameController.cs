668fdb7e3317f3bc4ca82d0f41ad98956352ca5b
d3adgoose <kayleem0201@gmail.com>
2026-01-28 18:19:29 -0800

    Add mask ScriptableObjects and update GameController

--- GameJamJan26/Assets/Scripts/GameController.cs
+++ GameJamJan26/Assets/Scripts/GameController.cs
@@ -1,6 +1,43 @@
 using UnityEngine;
 
-public class GameController
+public enum GameState
 {
-    
+    Observation,
+    Conversation,
+    Listening,
+    Speaking,
+    Reacting,
+    Win,
+    Lose
+}
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance;
+
+    public GameState currentState;
+
+    public int mistakes = 0;
+    public int maxMistakes = 1;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void ChangeState(GameState newState)
+    {
+        currentState = newState;
+        Debug.Log("State changed to: " + newState);
+    }
+
+    public void RegisterMistake()
+    {
+        mistakes++;
+
+        if (mistakes > maxMistakes)
+        {
+            ChangeState(GameState.Lose);
+        }
+    }
 }
---
+++ GameJamJan26/Assets/Scripts/MaskSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "TheMask/Mask")]
+public class MaskSO : ScriptableObject
+{
+    public string maskName;
+    public Sprite maskSprite;
+}
